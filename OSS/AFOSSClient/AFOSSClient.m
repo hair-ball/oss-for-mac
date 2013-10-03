20ec7712d4d7c60a5ed933ed8036851c58049cbb
undancer <undancer@gmail.com>
2013-10-04 01:09:00 +0800

    add Constants

--- OSS/AFOSSClient/AFOSSClient.m
+++ OSS/AFOSSClient/AFOSSClient.m
@@ -8,40 +8,42 @@
 
 #import "AFOSSClient.h"
 #import <CommonCrypto/CommonHMAC.h>
-static NSString * const AFAmazonS3ClientDefaultBaseURLString = @"http://oss.aliyuncs.com";
-NSString * const AFAmazonS3USStandardRegion = @"oss.aliyuncs.com";
-static NSData * AFHMACSHA1EncodedDataFromStringWithKey(NSString *string, NSString *key) {
+
+static NSString *const AFAmazonS3ClientDefaultBaseURLString = @"http://oss.aliyuncs.com";
+NSString *const AFAmazonS3USStandardRegion = @"oss.aliyuncs.com";
+
+static NSData *AFHMACSHA1EncodedDataFromStringWithKey(NSString *string, NSString *key) {
     NSData *data = [string dataUsingEncoding:NSASCIIStringEncoding];
     CCHmacContext context;
     const char *keyCString = [key cStringUsingEncoding:NSASCIIStringEncoding];
-    
+
     CCHmacInit(&context, kCCHmacAlgSHA1, keyCString, strlen(keyCString));
     CCHmacUpdate(&context, [data bytes], [data length]);
-    
+
     unsigned char digestRaw[CC_SHA1_DIGEST_LENGTH];
     NSInteger digestLength = CC_SHA1_DIGEST_LENGTH;
-    
+
     CCHmacFinal(&context, digestRaw);
-    
+
     return [NSData dataWithBytes:digestRaw length:digestLength];
 }
 
-static NSString * AFRFC822FormatStringFromDate(NSDate *date) {
+static NSString *AFRFC822FormatStringFromDate(NSDate *date) {
     NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     [dateFormatter setTimeZone:[NSTimeZone timeZoneWithName:@"GMT"]];
     [dateFormatter setDateFormat:@"EEE, dd MMM yyyy HH:mm:ss z"];
     [dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"]];
-    
+
     return [dateFormatter stringFromDate:date];
 }
 
-NSString * AFBase64EncodedStringFromData(NSData *data) {
+NSString *AFBase64EncodedStringFromData(NSData *data) {
     NSUInteger length = [data length];
     NSMutableData *mutableData = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
-    
-    uint8_t *input = (uint8_t *)[data bytes];
-    uint8_t *output = (uint8_t *)[mutableData mutableBytes];
-    
+
+    uint8_t *input = (uint8_t *) [data bytes];
+    uint8_t *output = (uint8_t *) [mutableData mutableBytes];
+
     for (NSUInteger i = 0; i < length; i += 3) {
         NSUInteger value = 0;
         for (NSUInteger j = i; j < (i + 3); j++) {
@@ -50,21 +52,22 @@ NSString * AFBase64EncodedStringFromData(NSData *data) {
                 value |= (0xFF & input[j]);
             }
         }
-        
+
         static uint8_t const kAFBase64EncodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-        
+
         NSUInteger idx = (i / 3) * 4;
         output[idx + 0] = kAFBase64EncodingTable[(value >> 18) & 0x3F];
         output[idx + 1] = kAFBase64EncodingTable[(value >> 12) & 0x3F];
-        output[idx + 2] = (i + 1) < length ? kAFBase64EncodingTable[(value >> 6)  & 0x3F] : '=';
-        output[idx + 3] = (i + 2) < length ? kAFBase64EncodingTable[(value >> 0)  & 0x3F] : '=';
+        output[idx + 2] = (i + 1) < length ? kAFBase64EncodingTable[(value >> 6) & 0x3F] : '=';
+        output[idx + 3] = (i + 2) < length ? kAFBase64EncodingTable[(value >> 0) & 0x3F] : '=';
     }
-    
+
     return [[NSString alloc] initWithData:mutableData encoding:NSASCIIStringEncoding];
 }
+
 @interface AFOSSClient ()
-@property (readwrite, nonatomic, copy) NSString *accessKey;
-@property (readwrite, nonatomic, copy) NSString *secret;
+@property(readwrite, nonatomic, copy) NSString *accessKey;
+@property(readwrite, nonatomic, copy) NSString *secret;
 
 - (void)setObjectWithMethod:(NSString *)method
                        file:(NSString *)filePath
@@ -74,26 +77,27 @@ NSString * AFBase64EncodedStringFromData(NSData *data) {
                     success:(void (^)(id responseObject))success
                     failure:(void (^)(NSError *error))failure;
 @end
+
 @implementation AFOSSClient
 @synthesize baseURL = _oss_baseURL;
 @synthesize bucket = _bucket;
 @synthesize region = _region;
 @synthesize accessKey = _accessKey;
 @synthesize secret = _secret;
+
 - (id)initWithAccessKeyID:(NSString *)accessKey
-                   secret:(NSString *)secret
-{
+                   secret:(NSString *)secret {
     self = [self initWithBaseURL:[NSURL URLWithString:AFAmazonS3ClientDefaultBaseURLString]];
     if (!self) {
         return nil;
     }
-    
+
     // Workaround for designated initializer of subclass
     self.baseURL = [NSURL URLWithString:AFAmazonS3ClientDefaultBaseURLString];
-    
+
     self.accessKey = accessKey;
     self.secret = secret;
-    
+
     return self;
 }
 
@@ -102,11 +106,12 @@ NSString * AFBase64EncodedStringFromData(NSData *data) {
     if (!self) {
         return nil;
     }
-    
+
     [self registerHTTPOperationClass:[AFXMLRequestOperation class]];
-    
+
     return self;
 }
+
 - (void)setBucket:(NSString *)bucket {
     [self willChangeValueForKey:@"baseURL"];
     [self willChangeValueForKey:@"bucket"];
@@ -114,91 +119,90 @@ NSString * AFBase64EncodedStringFromData(NSData *data) {
     [self didChangeValueForKey:@"bucket"];
     [self didChangeValueForKey:@"baseURL"];
 }
+
 - (NSURL *)baseURL {
     return [NSURL URLWithString:AFAmazonS3ClientDefaultBaseURLString];
 }
+
 - (NSDictionary *)authorizationHeadersForRequest:(NSMutableURLRequest *)request {
     if (self.accessKey && self.secret) {
         // Long header values that are subject to "folding" should split into new lines according to AWS's documentation.
-		NSMutableDictionary *mutableOSSHeaderFields = [NSMutableDictionary dictionary];
-		[[request allHTTPHeaderFields] enumerateKeysAndObjectsUsingBlock:^(NSString *key, id value, BOOL *stop) {
-			key = [key lowercaseString];
-			if ([key hasPrefix:@"x-oss"]) {
-				if ([mutableOSSHeaderFields objectForKey:key]) {
-					value = [[mutableOSSHeaderFields objectForKey:key] stringByAppendingFormat:@",%@", value];
-				}
-				[mutableOSSHeaderFields setObject:value forKey:key];
-			}
-		}];
-        
-		NSMutableString *mutableCanonicalizedOSSHeaderString = [NSMutableString string];
-		for (NSString *key in [[mutableOSSHeaderFields allKeys] sortedArrayUsingSelector:@selector(compare:)]) {
+        NSMutableDictionary *mutableOSSHeaderFields = [NSMutableDictionary dictionary];
+        [[request allHTTPHeaderFields] enumerateKeysAndObjectsUsingBlock:^(NSString *key, id value, BOOL *stop) {
+            key = [key lowercaseString];
+            if ([key hasPrefix:@"x-oss"]) {
+                if ([mutableOSSHeaderFields objectForKey:key]) {
+                    value = [[mutableOSSHeaderFields objectForKey:key] stringByAppendingFormat:@",%@", value];
+                }
+                [mutableOSSHeaderFields setObject:value forKey:key];
+            }
+        }];
+
+        NSMutableString *mutableCanonicalizedOSSHeaderString = [NSMutableString string];
+        for (NSString *key in [[mutableOSSHeaderFields allKeys] sortedArrayUsingSelector:@selector(compare:)]) {
             id value = [mutableOSSHeaderFields objectForKey:key];
-			[mutableCanonicalizedOSSHeaderString appendFormat:@"%@:%@\n", key, value];
-		}
-        
-        NSString *canonicalizedResource = [NSString stringWithFormat:@"%@%@", self.bucket?self.bucket:@"", request.URL.path];
-    	NSString *method = [request HTTPMethod];
-		NSString *contentMD5 = [request valueForHTTPHeaderField:@"Content-MD5"];
-		NSString *contentType = [request valueForHTTPHeaderField:@"Content-Type"];
-		NSString *date = AFRFC822FormatStringFromDate([NSDate date]);
-        
-		NSMutableString *mutableString = [NSMutableString string];
-		[mutableString appendFormat:@"%@\n", (method) ? method : @""];
-		[mutableString appendFormat:@"%@\n", (contentMD5) ? contentMD5 : @""];
-		[mutableString appendFormat:@"%@\n", (contentType) ? contentType : @""];
-		[mutableString appendFormat:@"%@\n", (date) ? date : @""];
-		[mutableString appendFormat:@"%@", mutableCanonicalizedOSSHeaderString];
-		[mutableString appendFormat:@"%@", canonicalizedResource];
-        
+            [mutableCanonicalizedOSSHeaderString appendFormat:@"%@:%@\n", key, value];
+        }
+
+        NSString *canonicalizedResource = [NSString stringWithFormat:@"%@%@", self.bucket ? self.bucket : @"", request.URL.path];
+        NSString *method = [request HTTPMethod];
+        NSString *contentMD5 = [request valueForHTTPHeaderField:@"Content-MD5"];
+        NSString *contentType = [request valueForHTTPHeaderField:@"Content-Type"];
+        NSString *date = AFRFC822FormatStringFromDate([NSDate date]);
+
+        NSMutableString *mutableString = [NSMutableString string];
+        [mutableString appendFormat:@"%@\n", (method) ? method : @""];
+        [mutableString appendFormat:@"%@\n", (contentMD5) ? contentMD5 : @""];
+        [mutableString appendFormat:@"%@\n", (contentType) ? contentType : @""];
+        [mutableString appendFormat:@"%@\n", (date) ? date : @""];
+        [mutableString appendFormat:@"%@", mutableCanonicalizedOSSHeaderString];
+        [mutableString appendFormat:@"%@", canonicalizedResource];
+
         NSData *hmac = AFHMACSHA1EncodedDataFromStringWithKey(mutableString, self.secret);
         NSString *signature = AFBase64EncodedStringFromData(hmac);
-        NSLog(@"DICT DICT %@",@{@"Authorization": [NSString stringWithFormat:@"OSS %@:%@", self.accessKey, signature],
-                                @"Date": (date) ? date : @""
-                                });
-        return @{@"Authorization": [NSString stringWithFormat:@"OSS %@:%@", self.accessKey, signature],
-                 @"Date": (date) ? date : @""
-                 };
+        NSLog(@"DICT DICT %@", @{@"Authorization" : [NSString stringWithFormat:@"OSS %@:%@", self.accessKey, signature],
+                @"Date" : (date) ? date : @""
+        });
+        return @{@"Authorization" : [NSString stringWithFormat:@"OSS %@:%@", self.accessKey, signature],
+                @"Date" : (date) ? date : @""
+        };
     }
-    
+
     return nil;
 }
 #pragma mark -
 
 - (void)enqueueOSSRequestOperationWithMethod:(NSString *)method
-                                       path:(NSString *)path
-                                 parameters:(NSDictionary *)parameters
-                                    success:(void (^)(id responseObject))success
-                                    failure:(void (^)(NSError *error))failure
-{
+                                        path:(NSString *)path
+                                  parameters:(NSDictionary *)parameters
+                                     success:(void (^)(id responseObject))success
+                                     failure:(void (^)(NSError *error))failure {
     NSURLRequest *request = [self requestWithMethod:method path:path parameters:parameters];
-    
+
     AFHTTPRequestOperation *requestOperation = [self HTTPRequestOperationWithRequest:request success:^(AFHTTPRequestOperation *operation, id responseObject) {
         if (success) {
-            NSLog(@"melon %@",[[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding]);
+            NSLog(@"melon %@", [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding]);
             success(responseObject);
         }
-    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+    }                                                                        failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         if (failure) {
             failure(error);
         }
     }];
-    
+
     [self enqueueHTTPRequestOperation:requestOperation];
 }
 
 #pragma mark Service Operations
 
 - (void)getBucketsWithSuccess:(void (^)(id responseObject))success
-                      failure:(void (^)(NSError *error))failure
-{
+                      failure:(void (^)(NSError *error))failure {
     [self enqueueOSSRequestOperationWithMethod:@"GET" path:@"/" parameters:nil success:success failure:failure];
 }
 
 - (void)getBucket:(NSString *)bucket
           success:(void (^)(id responseObject))success
-          failure:(void (^)(NSError *error))failure
-{
+          failure:(void (^)(NSError *error))failure {
     [self enqueueOSSRequestOperationWithMethod:@"GET" path:bucket parameters:nil success:success failure:failure];
 }
 
@@ -206,14 +210,13 @@ NSString * AFBase64EncodedStringFromData(NSData *data) {
 
 - (NSMutableURLRequest *)requestWithMethod:(NSString *)method
                                       path:(NSString *)path
-                                parameters:(NSDictionary *)parameters
-{
-	NSMutableURLRequest *request = [super requestWithMethod:method path:path parameters:parameters];
-    
+                                parameters:(NSDictionary *)parameters {
+    NSMutableURLRequest *request = [super requestWithMethod:method path:path parameters:parameters];
+
     [[self authorizationHeadersForRequest:request] enumerateKeysAndObjectsUsingBlock:^(id field, id value, BOOL *stop) {
         [request setValue:value forHTTPHeaderField:field];
     }];
-    
+
     return request;
 }
 @end
---
+++ OSS/AFOSSClient/OSSConstants.h
@@ -0,0 +1,65 @@
+//
+// Created by undancer on 13-10-4.
+// Copyright (c) 2013 ___FULLUSERNAME___. All rights reserved.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+#define kAUTHORIZATION                                  @"Authorization"
+#define kCACHE_CONTROL                                  @"Cache-Control"
+#define kCONTENT_DISPOSITION                            @"Content-Disposition"
+#define kCONTENT_ENCODING                               @"Content-Encoding"
+#define kCONTENT_LENGTH                                 @"Content-Length"
+#define kCONTENT_MD5                                    @"Content-MD5"
+#define kCONTENT_TYPE                                   @"Content-Type"
+#define kDATE                                           @"Date"
+#define kETAG                                           @"ETag"
+#define kEXPIRES                                        @"Expires"
+#define kHOST                                           @"Host"
+#define kLAST_MODIFIED                                  @"Last-Modified"
+#define kRANGE                                          @"Range"
+#define kLOCATION                                       @"Location"
+
+#define kOSS_PREFIX                                     @"x-oss-"
+#define kOSS_USER_METADATA_PREFIX                       @"x-oss-meta-"
+#define kOSS_CANNED_ACL                                 @"x-oss-acl"
+#define kSTORAGE_CLASS                                  @"x-oss-storage-class"
+#define kOSS_VERSION_ID                                 @"x-oss-version-id"
+#define kGET_OBJECT_IF_MODIFIED_SINCE                   @"If-Modified-Since"
+#define kGET_OBJECT_IF_UNMODIFIED_SINCE                 @"If-Unmodified-Since"
+#define kGET_OBJECT_IF_MATCH                            @"If-Match"
+#define kGET_OBJECT_IF_NONE_MATCH                       @"If-None-Match"
+#define kCOPY_OBJECT_SOURCE                             @"x-oss-copy-source"
+#define kCOPY_OBJECT_SOURCE_IF_MATCH                    @"x-oss-copy-source-if-match"
+#define kCOPY_OBJECT_SOURCE_IF_NONE_MATCH               @"x-oss-copy-source-if-none-match"
+#define kCOPY_OBJECT_SOURCE_IF_UNMODIFIED_SINCE         @"x-oss-copy-source-if-unmodified-since"
+#define kCOPY_OBJECT_SOURCE_IF_MODIFIED_SINCE           @"x-oss-copy-source-if-modified-since"
+#define kCOPY_OBJECT_METADATA_DIRECTIVE                 @"x-oss-metadata-directive"
+#define kOSS_HEADER_REQUEST_ID                          @"x-oss-request-id"
+
+
+#define kDEFAULT_ENCODING                               @"UTF-8"
+#define kALGORITHM                                      @"HmacSHA1"
+#define kVERSION                                        @"1"
+
+#define kDEFAULT_RETRY_PAUSE_SCALE                      @300
+#define kDEFAULT_MARK_LIMIT                             @4096
+
+#define kRFC822_DATE_FORMAT                             @"EEE, dd MMM yyyy HH:mm:ss z"
+#define kISO8601_DATE_FORMAT                            @"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+#define kALTERNATIVE_ISO8601_DATE_FORMAT                @"yyyy-MM-dd'T'HH:mm:ss'Z'"
+
+
+#define kRESOURCE_NAME_COMMON                           @"common"
+
+#define kDEFAULT_OBJECT_CONTENT_TYPE                    @"application/octet-stream"
+
+#define kLIST_PART_MAX_PARTS                            @1000
+#define kSUB_RESOURCE_UPLOADS                           @"uploads"
+
+#define kDEFAULT_OSS_ENDPOINT                           @"http://oss.aliyuncs.com"
+#define kCHARSET                                        @"utf-8"
+#define kDEFAULT_XML_CHARSET                            @"utf-8"
+#define kBUFFER_SIZE                                    @8192
+#define kMAX_FILE_SIZE                                   @5368709120L
+
